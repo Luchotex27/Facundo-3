3f0853a6153889e3b8602d971d3bf256aeb842dc
Luchotex27 <104880125+Luchotex27@users.noreply.github.com>
2025-02-21 05:56:05 -0300

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,15 @@
+const estado = document.querySelector('estado');
+console.log(estado);
+var i=0;
+if('estado'="APROBADO")
+{
+    i=1
+}
+else{
+    i=2
+}
+if(i=1){
+    document.estado.style.backgroundcolor ='red';
+
+    
+}
